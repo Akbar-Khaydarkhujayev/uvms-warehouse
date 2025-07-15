a0a1b19bc9474ce313b0d54282aec4eb00cbf3f5
Akbar-Khaydarkhujayev <akbarboss91@gmail.com>
2025-07-15 12:08:14 +0500

    acsa

--- src/sections/dashboard/api/arendators/editStatus.ts
+++ src/sections/dashboard/api/arendators/editStatus.ts
@@ -5,11 +5,7 @@ import axiosInstance from 'src/utils/axios';
 import { toast } from 'src/components/snackbar';
 
 export const editArendators = ({ id, status }: { id: string | number; status: boolean }) =>
-  axiosInstance.put(`Arendators/edit/status/${id}`, {
-    params: {
-      status,
-    },
-  });
+  axiosInstance.put(`Arendators/edit/status/${id}?status=${status}`);
 
 export const useEditArendators = () => {
   const queryClient = useQueryClient();
